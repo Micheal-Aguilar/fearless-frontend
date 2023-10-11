2f2c8fc237d3c8281dddcd919b3dd480d8ce92c7
Micheal <mikey4426@gmail.com>
2023-10-11 13:53:54 -0700

    fearless-frontend

--- attendees_microservice/attendees_bc/settings.py
+++ attendees_microservice/attendees_bc/settings.py
@@ -51,7 +51,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+]
+
 
 CRONJOBS = [
     ("* * * * *", "attendees.poll.get_conferences"),
